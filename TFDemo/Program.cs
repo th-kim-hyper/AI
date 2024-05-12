e8569fad345926e0e7cf9e6e236a28b8733a7382
th.kim <th.kim@hyperinfo.co.kr>
2024-05-13 08:17:15 +0900

    초기 커밋.

---
+++ TFDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Tensorflow;
+using Tensorflow.Keras;
+using static Tensorflow.Binding;
+using System.IO;
+using Tensorflow.Keras.Engine;
+using System;
+
+namespace TensorFlowNET.Examples
+{
+    /// <summary>
+    /// Simple hello world using TensorFlow
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = tf.constant("Hello, TensorFlow!");
+            Console.WriteLine(hello + ":"  + tf.VERSION);
+
+        }
+    }
+}
