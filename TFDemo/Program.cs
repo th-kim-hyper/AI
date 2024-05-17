edbe771bdba2d25530df4caacd0d9741da893c6d
th.kim <th.kim@hyperinfo.co.kr>
2024-05-17 17:07:05 +0900

    project init

--- TFDemo/Program.cs
+++ TFDemo/Program.cs
@@ -5,6 +5,7 @@ using static Tensorflow.Binding;
 using System.IO;
 using Tensorflow.Keras.Engine;
 using System;
+using Tensorflow.NumPy;
 
 namespace TensorFlowNET.Examples
 {
@@ -15,8 +16,21 @@ namespace TensorFlowNET.Examples
     {
         static void Main(string[] args)
         {
+
             var hello = tf.constant("Hello, TensorFlow!");
             Console.WriteLine(hello + ":"  + tf.VERSION);
+
+            // Create a tensor holds a scalar value
+            var t1 = new Tensor(3);
+
+            // Init from a string
+            var t2 = new Tensor("Hello! TensorFlow.NET");
+
+            // Tensor holds a ndarray
+            var nd = new NDArray(new int[] { 3, 1, 1, 2 });
+            var t3 = new Tensor(nd);
+
+            Console.WriteLine($"t1: {t1}, t2: {t2}, t3: {t3}");
 
         }
     }
